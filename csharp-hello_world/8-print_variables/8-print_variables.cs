93e3e1e0d90e5b07566b120a50ca463adbb0c072
amenallahg122 <bigpapiamen@gmail.com>
2024-08-04 18:31:56 +0200

    8-print_variables.cs

---
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 972;
+        string text = "Mission Street";
+        Console.WriteLine(number + " " + text);
+    }
+}
