f9705e6651c955ea9518209d4eb62eddaf7bd425
amenallahg122 <bigpapiamen@gmail.com>
2024-08-04 03:26:58 +0200

    6-print_comb2.cs

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string result = "";
+
+        for (int i = 0; i <= 9; i++)
+        {
+            for (int j = i + 1; j <= 9; j++)
+            {
+                result += i.ToString("D1") + j.ToString("D1");
+
+                if (!(i == 8 && j == 9))
+                {
+                    result += ", ";
+                }
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
