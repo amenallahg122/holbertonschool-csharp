5fb2c35dda7f0df42e96df358997b54cb0b3e69e
amenallahg122 <bigpapiamen@gmail.com>
2024-08-04 18:40:41 +0200

    9-print_specifiers.cs

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double percent = 0.7553;
+        double currency = 98765.43;
+        Console.WriteLine($"Percent: {percent:P2} tip");
+        Console.WriteLine($"Currency: {currency:C2}");
+    }
+}
