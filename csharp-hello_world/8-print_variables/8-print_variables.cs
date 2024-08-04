75335426cb6214522762f1172ade1b3f275dd778
amenallahg122 <bigpapiamen@gmail.com>
2024-08-04 18:35:04 +0200

    8-print_variables.cs

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -4,8 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        int number = 972;
-        string text = "Mission Street";
-        Console.WriteLine(number + " " + text);
+        string str1 = "Street";
+        string str2 = "Mission";
+        int int1 = 972;
+        Console.WriteLine(int1 + " " + str2 + " " + str1);
     }
 }
