0d7a502d6009e5fb1c55a678f19bb0eea3f32ef6
amenallahg122 <bigpapiamen@gmail.com>
2024-08-25 23:26:29 +0200

    0-dog.cs

---
+++ csharp-structs_enums/0-dog/0-dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rating rating;
+
+        rating = Rating.Great;
+
+        Console.WriteLine("Score: " + rating);
+        Console.WriteLine("Score: " + (int)rating);
+    }
+}
